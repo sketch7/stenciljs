81303b7cd671f9eeb9518527d03fb8811ffade33
Kurt Aquilina <k.aquilina92@betssongroup.com>
2026-05-19 16:32:35 +0200

    update

--- libs/stencil-ui/src/compose/compose.tsx
+++ libs/stencil-ui/src/compose/compose.tsx
@@ -3,8 +3,8 @@ import { Component, Event, EventEmitter, Listen, Prop, h } from "@stencil/core";
 import type { VNode } from "@stencil/core";
 
 import { isComposeDevEnv } from "./registry/is-compose-dev";
-import { useCompositionRegistry } from "./registry/use-composition-registry";
 import type { ComposeEventDetail } from "./types";
+import { useCompositionRegistry } from "./registry/registry";
 
 @Component({
 	tag: "ssv-compose",

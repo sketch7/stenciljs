f57182df980f46de2c5068bfc7c367c8f3a37959
Stephen Lautier <stephen.lautier@outlook.com>
2026-06-01 09:29:42 +0200

    fix warns

--- apps/stencil-playground/src/examples/ts-query/index.ts
+++ apps/stencil-playground/src/examples/ts-query/index.ts
@@ -3,6 +3,3 @@ export type { Post as TsQueryPost } from "./posts/posts.api";
 export { AppTsQueryPostsSignals } from "./posts-signals/posts-signals";
 export { AppTsQueryPrefetchDemo } from "./prefetch/prefetch-demo";
 export { AppTsQueryUseQueriesDemo } from "./use-queries/use-queries-demo";
-export { AppTsQueryUseQueriesInline } from "./use-queries/use-queries-inline";
-export { AppTsQueryUseQueriesReuse } from "./use-queries/use-queries-reuse";
-export { AppTsQueryUseQueriesSignals } from "./use-queries/use-queries-signals";
